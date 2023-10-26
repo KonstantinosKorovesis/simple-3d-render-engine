779bf1d256c4332c6c668ef27c3bbf0ae98c13f2
= <t8210063@aueb.gr>
2023-10-26 07:22:16 +0300

    Differentiate 2D and 3D shapes, add new classes
    v1.2 -
    Added classes:
     - Shape2D, to distinguish between 2D and 3D shapes.
     - Rectangle, for connecting 4 Vertices, with method triangulate() for turning Rectangles into two Triangles.
     - Shape3D, a stand-alone class for creating 3D Shapes using 2D Shapes, with methods tetrahedron(), cube().
     - Draw, which handles all the drawing, with methods drawTriangles and drawRectangles for displaying different 3D Shapes.

--- DemoViewer.java
+++ DemoViewer.java
@@ -6,18 +6,22 @@ import java.util.ArrayList;
 
 public class DemoViewer {
     public static void main(String[] args) {
-        //initialize frame(window) and the pane everything will be added in
+        //initialize frame (window)
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+        
+        //the pane everything will be added in
         Container pane = frame.getContentPane();
         pane.setLayout(new BorderLayout());
-        
+
         //horizontal slider
-        JSlider headingSlider = new JSlider(0,360,180);  //value=180 is slider's starting position, the middle
+        JSlider headingSlider = new JSlider(-180, 180, 0);
         pane.add(headingSlider, BorderLayout.SOUTH);
 
         //vertical slider
-        JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL, -90, 90, 0);
+        JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL, 90, 270, 180);
         pane.add(pitchSlider, BorderLayout.EAST);
         
         //panel for render results
@@ -55,50 +59,23 @@ public class DemoViewer {
                 //transformation matrix for both XZ and YZ rotations
                 Matrix3 transform = headingTransform.multiply(pitchTransform);
 
-                //make List<Triangle> and add 4 Triangles forming a basic tetrahedron
-                List<Triangle> tris = new ArrayList<Triangle>();
-                tris.add(new Triangle(new Vertex(100, 100, 100),
-                                    new Vertex(-100, -100, 100),
-                                    new Vertex(-100, 100, -100),
-                                    Color.WHITE));
-                tris.add(new Triangle(new Vertex(100, 100, 100),
-                                    new Vertex(-100, -100, 100),
-                                    new Vertex(100, -100, -100),
-                                    Color.RED));
-                tris.add(new Triangle(new Vertex(-100, 100, -100),
-                                    new Vertex(100, -100, -100),
-                                    new Vertex(100, 100, 100),
-                                    Color.GREEN));
-                tris.add(new Triangle(new Vertex(-100, 100, -100),
-                                    new Vertex(100, -100, -100),
-                                    new Vertex(-100, -100, 100),
-                                    Color.BLUE));
-                
-                //set drawing color to white
-                g2.setColor(Color.WHITE);
-
-                for (Triangle t : tris) {
-                    //transform all Triangle vertices based on current rotation 
-                    Vertex v1 = transform.transform(t.v1);
-                    Vertex v2 = transform.transform(t.v2);
-                    Vertex v3 = transform.transform(t.v3);
-
-                    //draw them using orthographic projection
-                    Path2D path = new Path2D.Double();
-                    path.moveTo(v1.x, v1.y);
-                    path.lineTo(v2.x, v2.y);
-                    path.lineTo(v3.x, v3.y);
-                    path.closePath();
-                    g2.draw(path);
+                int choice = 1;
+                switch (choice) {
+                    //tetrahedron
+                    case 1:
+                        List<Triangle> tris = Shape3D.tetrahedron();
+                        Draw.drawTriangles(tris, transform, g2);
+                        break;
+                    //cube
+                    case 2:
+                        List<Rectangle> recs = Shape3D.cube();
+                        Draw.drawRectangles(recs, transform, g2);
+                        break;
                 }
             }
         };
 
         pane.add(renderPanel, BorderLayout.CENTER);
-        
-        //final settings
-        frame.setSize(400, 400);
-        frame.setVisible(true);
         
         //add action listeners to the sliders
         headingSlider.addChangeListener(e -> renderPanel.repaint());
@@ -107,7 +84,7 @@ public class DemoViewer {
 }
 
 class Vertex {
-    //x is left-right, y is up-down, z is depth (larger z means closer to observer)
+    //x is left-right, y is up-down, z is depth (positive z means away from observer)
     double x, y, z;
     Vertex(double x, double y, double z) {
         this.x = x;
@@ -120,18 +97,118 @@ class Vertex {
     } 
 }
 
-class Triangle {
-    Vertex v1, v2, v3;
+class Shape2D {
     Color color;
+    Shape2D(Color color) {
+        this.color = color;
+    }
+}
+
+class Triangle extends Shape2D {
+    Vertex v1, v2, v3;
     Triangle(Vertex v1, Vertex v2, Vertex v3, Color color) {
+        super(color);
         this.v1 = v1;
         this.v2 = v2;
         this.v3 = v3;
-        this.color = color;
     }
     @Override
     public String toString() {
         return String.format("%s,\n%s,\n%s\n", v1.toString(), v2.toString(), v3.toString());
+    } 
+}
+
+class Rectangle extends Shape2D {
+    Vertex v1, v2, v3, v4;
+    Rectangle(Vertex v1, Vertex v2, Vertex v3, Vertex v4, Color color) {
+        super(color);
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+        this.v4 = v4;
+    }
+    List<Triangle> triangulate() {
+        List<Vertex> vers = new ArrayList<Vertex>();
+        vers.add(v1);
+        vers.add(v2);
+        vers.add(v3);
+        vers.add(v4);
+        double maxDistance = 0;
+        Vertex diagAcross = v1;
+        for (Vertex v : vers) {
+            if (maxDistance < Math.sqrt(Math.pow(v1.x - v.x, 2) + Math.pow(v1.y - v.y, 2) + Math.pow(v1.z - v.z, 2))) {
+                maxDistance = Math.sqrt(Math.pow(v1.x - v.x, 2) + Math.pow(v1.y - v.y, 2) + Math.pow(v1.z - v.z, 2));
+                diagAcross = v;
+            }
+        }
+        vers.remove(v1);
+        vers.remove(diagAcross);
+        List<Triangle> tris = new ArrayList<Triangle>();
+        tris.add(new Triangle(v1, vers.get(0), diagAcross, color));
+        tris.add(new Triangle(diagAcross, vers.get(1), v1, color));
+        return tris;
+    }
+    @Override
+    public String toString() {
+        return String.format("%s,\n%s,\n%s,\n%s\n", v1.toString(), v2.toString(), v3.toString(), v4.toString());
+    }
+}
+
+class Shape3D {
+    //returns a List of Triangles that form a standard tetrahedron
+    static List<Triangle> tetrahedron() {
+        List<Triangle> t = new ArrayList<Triangle>();
+        t.add(new Triangle(new Vertex(100, 100, 100),
+                            new Vertex(-100, -100, 100),
+                            new Vertex(-100, 100, -100),
+                            Color.RED));
+        t.add(new Triangle(new Vertex(100, 100, 100),
+                            new Vertex(-100, -100, 100),
+                            new Vertex(100, -100, -100),
+                            Color.GREEN));
+        t.add(new Triangle(new Vertex(-100, 100, -100),
+                            new Vertex(100, -100, -100),
+                            new Vertex(100, 100, 100),
+                            Color.BLUE));
+        t.add(new Triangle(new Vertex(-100, 100, -100),
+                            new Vertex(100, -100, -100),
+                            new Vertex(-100, -100, 100),
+                            Color.YELLOW));
+        return t;
+    }
+    static List<Rectangle> cube() {
+        List<Rectangle> r = new ArrayList<Rectangle>();
+        r.add(new Rectangle(new Vertex(-100, 100, 100),
+                            new Vertex(-100, -100, 100),
+                            new Vertex(100, -100, 100),
+                            new Vertex(100, 100, 100),
+                            Color.RED));
+        r.add(new Rectangle(new Vertex(-100, 100, -100),
+                            new Vertex(-100, -100, -100),
+                            new Vertex(100, -100, -100),
+                            new Vertex(100, 100, -100),
+                            Color.RED));
+        r.add(new Rectangle(new Vertex(-100, 100, 100),
+                            new Vertex(-100, -100, 100),
+                            new Vertex(-100, -100, -100),
+                            new Vertex(-100, 100, -100),
+                            Color.GREEN));
+        r.add(new Rectangle(new Vertex(100, 100, 100),
+                            new Vertex(100, -100, 100),
+                            new Vertex(100, -100, -100),
+                            new Vertex(100, 100, -100),
+                            Color.GREEN));
+        r.add(new Rectangle(new Vertex(-100, 100, 100),
+                            new Vertex(100, 100, 100),
+                            new Vertex(100, 100, -100),
+                            new Vertex(-100, 100, -100),
+                            Color.BLUE));
+        r.add(new Rectangle(new Vertex(-100, -100, 100),
+                            new Vertex(100, -100, 100),
+                            new Vertex(100, -100, -100),
+                            new Vertex(-100, -100, -100),
+                            Color.BLUE));
+        return r;
     } 
 }
 
@@ -159,5 +236,35 @@ class Matrix3 {
             v.x * values[1] + v.y * values[4] + v.z * values[7],
             v.x * values[2] + v.y * values[5] + v.z * values[8]
         );
+    }
+}
+
+class Draw {
+    //receives Triangles, the transformation matrix object and the g2 object to draw on
+    static void drawTriangles(List<Triangle> tris, Matrix3 transform, Graphics2D g2) {
+        g2.setColor(Color.WHITE);
+        Path2D path = new Path2D.Double();
+        for (Triangle t : tris) {
+            Vertex v1 = transform.transform(t.v1);
+            Vertex v2 = transform.transform(t.v2);
+            Vertex v3 = transform.transform(t.v3);
+            path.moveTo(v1.x, v1.y);
+            path.lineTo(v2.x, v2.y);
+            path.lineTo(v3.x, v3.y);
+            path.closePath();
+            g2.draw(path);
+            path.reset();
+        }
+    }
+    //receives Rectangles which are turned into Triangles and sent to the drawTriangles method
+    static void drawRectangles(List<Rectangle> recs, Matrix3 transform, Graphics2D g2) {
+        List<Triangle> tris = new ArrayList<Triangle>();
+        List<Triangle> temp = new ArrayList<Triangle>();
+        for (Rectangle r : recs) {
+            temp = r.triangulate();
+            tris.add(temp.get(0));
+            tris.add(temp.get(1));
+        }
+        drawTriangles(tris, transform, g2);
     }
 }
